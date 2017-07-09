f994429a0a29907c4d8e89ac6ee9ccff36870d4a
Erik Schoster <ejschoster@gmail.com>
2017-07-08 19:43:56 -0500

    first pass voice list...

--- astrid/ui/ide.py
+++ astrid/ui/ide.py
@@ -1,13 +1,18 @@
-from kivy.app import App
 import random
-from pippi import dsp, oscs, interpolation
+
+from kivy.app import App
+from kivy.extras.highlight import PythonLexer
+from kivy.garden.graph import Graph, MeshLinePlot, SmoothLinePlot
+from kivy.graphics import Color, Rectangle
 from kivy.properties import ListProperty, StringProperty
+from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.codeinput import CodeInput
 from kivy.uix.gridlayout import GridLayout
-from kivy.garden.graph import Graph, MeshLinePlot, SmoothLinePlot
-from kivy.extras.highlight import PythonLexer
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+from pippi import dsp, oscs, interpolation
 
 INITIAL_SCRIPT= """\
 from pippi import dsp, oscs
@@ -29,6 +34,89 @@ def play(ctl):
     return out
 """
 
+class Waveform(BoxLayout):
+    def __init__(self, sound, **kwargs):
+        super().__init__(**kwargs)
+        graph_size_x = 1024
+        graph_size_y = 60
+
+        self.orientation='vertical'
+        self.size_hint=(1, None)
+        self.height=graph_size_y
+
+        colors = []
+        for _ in range(sound.channels):
+            colors += [ [random.triangular(0.5, 1), random.triangular(0.5, 1), random.triangular(0.5, 1)] ]
+
+        for channel in range(sound.channels):
+            graph = Graph(
+                    x_ticks_minor=10, 
+                    x_ticks_major=100, 
+                    y_ticks_major=1, 
+                    y_grid_label=False, 
+                    x_grid_label=False, 
+                    padding=0, 
+                    x_grid=True, 
+                    draw_border=False, 
+                    y_grid=False, 
+                    xmin=-0, 
+                    xmax=graph_size_x, 
+                    ymin=-1, 
+                    ymax=1,
+                    size_hint=(1, 1), 
+                )
+
+            plot = SmoothLinePlot(color=colors[channel])
+            points = [ sound[x][channel] for x in range(len(sound)) ]
+            points = interpolation.linear(points, graph_size_x)
+            points = [ (x, point) for x, point in enumerate(points) ]
+            plot.points = points
+            graph.add_plot(plot)
+            self.add_widget(graph)
+
+def make_sound():
+    osc = oscs.Osc()
+    osc.amp = 0.75
+    length = 4000
+
+    osc.freq = 80
+    left = osc.play(length)
+    left = left.pan(0)
+    osc.freq = 100
+    right = osc.play(length)
+    right = right.pan(1)
+    out = dsp.mix([ left, right ])
+
+    return out
+
+class Voice(Label):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.font_name = '../../files/fonts/terminus/terminus.ttf'
+        self.font_size = 12
+        self.halign = 'left'
+        self.valign = 'center'
+        self.markup = True
+        self.color = [1,1,1,1]
+        self.disabled_color = [0.8, 0.8, 0.8, 1]
+        self.size = self.texture_size
+
+    def on_size(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(0.1, 0.1, random.triangular(0.2, 0.3), 1)
+            Rectangle(pos=self.pos, size=self.size)
+
+class VoiceList(BoxLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.orientation = 'vertical'
+
+        for _ in range(random.randint(3, 12)):
+            params = ' '.join([ '{}:{}'.format(random.choice(['o', 'd', 'f', 'a', 'v']), random.randint(1, 20)) for _ in range(random.randint(3, 8)) ])
+            label = Voice(text='voice [b]{}[/b]'.format(params))
+            self.add_widget(label)
+
 class Codebox(BoxLayout):
     def __init__(self, *args, **kwargs):
         super(Codebox, self).__init__(*args, **kwargs)
@@ -46,7 +134,7 @@ class Codebox(BoxLayout):
 class Console(BoxLayout):
     def __init__(self, *args, **kwargs):
         kwargs.update({'orientation': 'vertical'})
-        super(Console, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         messages = TextInput(text='MESSAGES', multiline=True, focus=False, size_hint=(1, 1), readonly=True)
         commands = TextInput(text='^_- ', multiline=False, size_hint=(1, None), height=25, readonly=False)
@@ -70,57 +158,17 @@ class Astrid(App):
         return self.run()
 
     def build(self):
-        graph_size_x = 1024
-        graph_size_y = 100
-
         layout = GridLayout(cols=2)
         right_pane = BoxLayout(orientation='vertical')
         codebox = Codebox()
         console = Console()
+        voicelist = VoiceList()
 
-        osc = oscs.Osc()
-        osc.amp = 0.75
-        length = 4000
+        sound = make_sound()
+        graph = Waveform(sound)
+        right_pane.add_widget(graph)
 
-        osc.freq = 80
-        left = osc.play(length)
-        left = left.pan(0)
-        osc.freq = 100
-        right = osc.play(length)
-        right = right.pan(1)
-        out = dsp.mix([ left, right ])
-
-        colors = []
-        for _ in range(out.channels):
-            colors += [ [random.triangular(0.5, 1), random.triangular(0.5, 1), random.triangular(0.5, 1)] ]
-
-        for channel in range(out.channels):
-            graph = Graph(
-                    x_ticks_minor=10, 
-                    x_ticks_major=100, 
-                    y_ticks_major=1, 
-                    y_grid_label=False, 
-                    x_grid_label=False, 
-                    padding=0, 
-                    x_grid=True, 
-                    draw_border=False, 
-                    y_grid=False, 
-                    xmin=-0, 
-                    xmax=graph_size_x, 
-                    ymin=-1, 
-                    ymax=1,
-                    size_hint=(1, None), 
-                    height=graph_size_y
-                )
-
-            plot = SmoothLinePlot(color=colors[channel])
-            points = [ out[x][channel] for x in range(len(out)) ]
-            points = interpolation.linear(points, graph_size_x)
-            points = [ (x, point) for x, point in enumerate(points) ]
-            plot.points = points
-            graph.add_plot(plot)
-            right_pane.add_widget(graph)
-
+        right_pane.add_widget(voicelist)
         right_pane.add_widget(console)
 
         layout.add_widget(codebox)
